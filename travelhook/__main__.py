8e7166375e323c7f578c25e6e7733ac3f63e5801
Cassidy Dingenskirchen <admin@15318.de>
2023-10-14 19:15:30 +0200

    reports are reaching me that the european rail network has succumbed to the vile disease of neoliberalism

--- travelhook/__main__.py
+++ travelhook/__main__.py
@@ -521,7 +521,7 @@ async def walk(
 @bot.tree.command(guilds=servers)
 async def pleasegivemetraintypes(ia):
     "print all the train types the bot knows about"
-    fv = ["D", "EC", "ECE", "EN", "IC", "ICE", "IR", "NJ", "RJ", "RJX", "TGV", "WB"]
+    fv = ["D", "EC", "ECE", "EN", "FLX", "IC", "ICE", "IR", "NJ", "RJ", "RJX", "TGV", "WB"]
     regio = ["CJX", "IRE", "MEX", "R", "RB", "RE", "REX", "TER"]
     sbahn = ["ATS", "L", "RER", "RS", "S"]
     transit = ["AST", "Bus", "Fähre", "M", "O-Bus", "RUF", "STB", "STR", "Tram", "U"]
